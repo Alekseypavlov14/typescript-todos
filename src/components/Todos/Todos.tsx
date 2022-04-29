1b3e8a17abc6df1707f5151dc8a0c02e3d30d480
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-04-29 22:52:29 +0300

    add Todos

---
+++ src/components/Todos/Todos.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react"
+
+interface TodosInterface {}
+
+const Todos: FC<TodosInterface> = () => {
+    return (
+        <div className='Todos Palette'>
+        </div>
+    )
+}
+
+export { Todos }
