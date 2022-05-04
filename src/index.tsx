3ae4fefabe36ad41a9a3fdf459734bd9d164083c
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-04 11:29:40 +0300

    add clear of localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,10 @@ root.render(
   </React.StrictMode>
 )
 
-window.onerror = (e) => alert(e)
+window.onerror = (e) => {
+  localStorage.setItem('todos', '[]')
+  alert(e)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
